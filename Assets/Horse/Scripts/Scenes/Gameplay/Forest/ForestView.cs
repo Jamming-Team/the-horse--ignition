d947c2d8299c8f0c89b018ee8b2b81e7852cb859
Xidriel <stormingisle@proton.me>
2025-07-26 23:54:53 +0300

    Timer visuals

--- Assets/Horse/Scripts/Scenes/Gameplay/Forest/ForestView.cs
+++ Assets/Horse/Scripts/Scenes/Gameplay/Forest/ForestView.cs
@@ -1,17 +1,23 @@
 using System;
 using System.Collections.Generic;
 using UnityEngine;
+using XTools;
 
 namespace Horse {
     public class ForestView : MonoBehaviour {
         [SerializeField] GameObject _viewPartsRoot;
+        [SerializeField] Transform _mask;
+        [SerializeField] Transform _startMaskPos;
+        [SerializeField] Transform _endMaskPos;
 
-        List<ForestViewPart> _viewParts = new List<ForestViewPart>();
+        List<GameObject> _fireParts = new List<GameObject>();
         int _curIndex;
 
         void Awake() {
-            foreach (Transform child in _viewPartsRoot.transform) {
-                _viewParts.Add(child.GetComponent<ForestViewPart>());
+            _viewPartsRoot.SetActive(true);
+            foreach (var child in _viewPartsRoot.transform.Children()) {
+                _fireParts.Add(child.gameObject);
+                child.gameObject.SetActive(false);
             }
 
             _curIndex = 0;
@@ -20,11 +26,13 @@ namespace Horse {
         public void AdjustFillAmount(float amount) {
             // Debug.Log("Count: " + _viewParts.Count);
             // Debug.Log("this: " + _curIndex / (_viewParts.Count - 1) + "; amount:" + amount);
-            var curViewAmount = Mathf.InverseLerp(0, _viewParts.Count - 1, _curIndex);
+            var curViewAmount = Mathf.InverseLerp(0, _fireParts.Count - 1, _curIndex);
             if (curViewAmount <= amount) {
-                _viewParts[_curIndex].Check();
-                _curIndex++; 
+                _fireParts[_curIndex].gameObject.SetActive(true);
+                _curIndex++;
             }
+            
+            _mask.position = Vector3.Lerp(_startMaskPos.position, _endMaskPos.position, curViewAmount);
         }
     }
 }
