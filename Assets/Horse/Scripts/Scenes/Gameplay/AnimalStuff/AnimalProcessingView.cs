119322f46c150d46e0b84be7c41afe4a0991aef4
Xidriel <stormingisle@proton.me>
2025-07-27 17:59:30 +0300

    Added clear text to the list

--- Assets/Horse/Scripts/Scenes/Gameplay/AnimalStuff/AnimalProcessingView.cs
+++ Assets/Horse/Scripts/Scenes/Gameplay/AnimalStuff/AnimalProcessingView.cs
@@ -6,19 +6,23 @@ namespace Horse {
         [SerializeField] AnimalViewLogItem _logItemViewPrefab;
         [SerializeField] GameObject _logItemsViewRoot;
 
+        [SerializeField] GameObject _noOneText;
+
         List<AnimalViewLogItem> _shownLogItems = new List<AnimalViewLogItem>();
-        
+
         public void FillView(List<AnimalEatingLog> logItems) {
             foreach (var item in _shownLogItems.ToArray()) {
                 item.Break();
             }
+
             _shownLogItems.Clear();
+
+            _noOneText.SetActive(!(logItems.Count > 0));
             foreach (var logItem in logItems) {
                 var item = Instantiate(_logItemViewPrefab, _logItemsViewRoot.transform);
                 item.Initialize(logItem);
                 _shownLogItems.Add(item);
             }
         }
-
     }
 }
